dfdbbdbbf948f176e6f3f189dc7d6f0cfa4a0933
fadillarizky <fadillarizky294@gmail.com>
2022-09-05 14:55:54 +0700

    Hapus DetailBeritaResponse.dart  ListBeritaResponse.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,20 +1,17 @@
 import 'dart:convert';
 
 import 'package:http/http.dart'as http;
-import 'package:portal_berita/api/DetailBeritaResponse.dart';
 import 'package:portal_berita/api/GetDetailResponse.dart';
 import 'package:portal_berita/api/GetListBeritaResponse.dart';
-import 'package:portal_berita/api/ListBeritaResponse.dart' ;
-
-
-import 'ListBeritaResponse.dart';
 
 // buat variabel konstanta dan global sebagai url api dasar
-const BASE_URL = "https://fadillarizky.enricko.com/api";
+  //LOCAL_URL = "http://localhost/web_portal_berita/api";
+// HOSTING_URL = "https://fadillarizky.enricko.com/api";
+const BASE_URL = "http://localhost/web_portal_berita/api";
 
 class Api {
 
-  static String IMG_URL = "https://fadillarizky.enricko.com/images/";
+  static String IMG_URL = "http://localhost/web_portal_berita/api";
   // membuat function dengan keyword static
   // Future = mendapat nilai dimasa yang akan datang setelah melakukan request api
   // var response = untuk melakukan request kemudian menunggu (await) response dari webservice
